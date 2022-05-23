0899c5264257244a1d3be24af8c33a3b4a3c02d4
Angeltromper <atromper@outlook.com>
2022-05-23 23:57:32 +0200

    huiswerk-les-3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { useState } from 'react';
+import {Route, Switch,Redirect} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import BlogPost from "./pages/BlogPost";
+import Login from ' /pages/Login';
+import Navigation from './components/Navigation';
+import BlogOverview from './pages/BlogOverview';
+
+
+
+function PrivateRoute({children, isAuth , ...rest}) {
+    return (
+        <Route {...rest}>
+            {isAuth ? children : <Redirect to="/login"/>}
+        </Route>
+    )
+}
 
 function App() {
-  // We houden in de state bij of iemand is "ingelogd" (simpele versie)
-  const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
+    // We houden in de state bij of iemand is "ingelogd" (simpele versie)
+    const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
 
-  return (
-    <div>
-      Maak hier jouw prachtige blog-applicatie!
-    </div>
-  );
+
+    return (
+        <div>
+            <Navigation isAuth={isAuthenticated} toggleAuth={toggleIsAuthenticated} />
+            <Switch>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+                <Route path="/login">
+                    <Login toggleAuth={toggleIsAuthenticated} />
+                </Route>
+                <PrivateRoute exact path="/blogposts" isAuth={isAuthenticated}>
+                    <BlogOverview />
+                </PrivateRoute>
+                <PrivateRoute exact path="/blog/:Id" isAuth={isAuthenticated}>
+                    <BlogPost />
+                </PrivateRoute>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section>
+            <h1>Home pagina</h1>
+            <h2>Welkom op een fantastisch blogplatform</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deserunt dolore, incidunt
+                nemo placeat saepe. Illo ipsum minus modi! Mollitia?</h3>
+        </section>
+    );
+}
+
+export default Home;
+
+
